29fc09d0df0e3e7429ab10464d3c21745deb5d1a
StephenChan <stephenjchan@gmail.com>
2026-01-21 14:46:05 -0800

    Add setting corresponding to spacer ref-set max size.

--- mermaid_classifier/pyspacer/settings.py
+++ mermaid_classifier/pyspacer/settings.py
@@ -47,6 +47,9 @@ class Settings(BaseSettings):
     mlflow_tracking_server: str | None = None
     training_inputs_percent_missing_allowed: int = 0
     spacer_extractors_cache_dir: str | None = None
+    # Yes, this is str. After it gets through the settings machinery,
+    # pyspacer will convert to int.
+    spacer_ref_set_max_size: str | None = None
     mlflow_http_request_max_retries: str | None = None
     mlflow_default_experiment_name: str | None = None
 
@@ -68,6 +71,15 @@ def set_env_vars_for_packages():
         # were downloaded from S3 or from a URL.
         # This is required if loading weights from such a source.
         spacer_extractors_cache_dir='SPACER_EXTRACTORS_CACHE_DIR',
+        # This pyspacer training setting is:
+        # - A cap on the reference set size, as a number of point-features
+        # - The size of batches used during training
+        # Raising this can better accommodate rare classes in large training
+        # runs, and can improve the trainer's ability to calibrate between
+        # epochs.
+        # However, this setting is also tied to training's memory usage,
+        # so monitor memory usage when increasing this setting.
+        spacer_ref_set_max_size='SPACER_TRAINING_BATCH_LABEL_COUNT',
         aws_region='SPACER_AWS_REGION',
         # If True, AWS is accessed without any credentials, which can
         # simplify setup while still allowing access to public S3 files.
