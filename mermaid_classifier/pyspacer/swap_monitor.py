58774b9a838b079242d76053a95a111936ebd3ea
Greg Nieuwenhuis <26285069+gnieuwenhuis@users.noreply.github.com>
2026-02-20 12:27:16 -0700

    add swap monitoring

---
+++ mermaid_classifier/pyspacer/swap_monitor.py
@@ -0,0 +1,14 @@
+import psutil
+from mlflow.system_metrics.metrics.base_metrics_monitor import BaseMetricsMonitor
+
+
+class SwapMonitor(BaseMetricsMonitor):
+    """Monitor swap memory usage for MLflow system metrics logging."""
+
+    def collect_metrics(self):
+        swap = psutil.swap_memory()
+        self._metrics["swap_usage_megabytes"].append(swap.used / 1e6)
+        self._metrics["swap_usage_percentage"].append(swap.percent)
+
+    def aggregate_metrics(self):
+        return {k: round(sum(v) / len(v), 1) for k, v in self._metrics.items()}
